164fd09179afdde6253af89229ae576ccd863584
Nishat Mukherji <nmukh1@gmail.com>
2024-09-08 16:40:19 -0500

    add Scene class skeleton with method placeholders

--- rotateglfw.py
+++ rotateglfw.py
@@ -16,3 +16,32 @@ import math
 
 import numpy as np
 
+
+class Scene:
+    """OpenGL 3D scene class"""
+    def __init__(self, vertex_data=None):
+        pass
+    
+    def init_shaders(self):
+        """Load shaders and get uniform locations."""
+        pass
+    
+    def init_buffers(self, vertex_data):
+        """Set up VAO and buffers."""
+        pass
+    
+    def init_textures(self):
+        """Load textures."""
+        pass    
+    
+    def render(self):
+        """Render the scene."""
+        pass    
+    
+    def step(self):
+        """Update the scene."""
+        pass
+    
+
+    
+    
