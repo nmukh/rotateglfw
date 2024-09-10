87511d48909ac869946d7fee36a2a3a308b7f6f1
Nishat Mukherji <nmukh1@gmail.com>
2024-09-10 12:24:41 -0500

    modify shader to use texture coordinates

--- vertex_shader.glsl
+++ vertex_shader.glsl
@@ -1,6 +1,7 @@
 #version 410 core
 
 layout(location = 0) in vec3 aVert;
+layout(location = 1) in vec2 aTexCoord;
 
 uniform mat4 uMVMatrix;
 uniform mat4 uPMatrix;
@@ -9,12 +10,15 @@ uniform float uTheta;
 out vec2 vTexCoord;
 
 void main() {
-  mat4 rot = mat4(
-        vec4(1.0,  0.0,          0.0,         0.0),
-        vec4(0.0,  cos(uTheta),  sin(uTheta), 0.0),
-        vec4(0.0, -sin(uTheta),  cos(uTheta), 0.0),
-        vec4(0.0,  0.0,          0.0,         1.0)
-  );
-  gl_Position = uPMatrix * uMVMatrix * rot * vec4(aVert, 1.0);
-  vTexCoord = aVert.xy + vec2(0.5, 0.5);
+    // Rotation matrix for the Y-axis
+    mat4 rot = mat4(
+          vec4(cos(uTheta),  0.0, -sin(uTheta), 0.0),
+          vec4(0.0,         -1.0,  0.0,         0.0),
+          vec4(sin(uTheta),  0.0,  cos(uTheta), 0.0),
+          vec4(0.0,          0.0,  0.0,         1.0)
+    );
+    gl_Position = uPMatrix * uMVMatrix * rot * vec4(aVert, 1.0);
+
+    // Pass the texture coordinates to the fragment shader
+    vTexCoord = aTexCoord;
 }
